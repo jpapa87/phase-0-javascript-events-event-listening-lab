72cf263b185e6f5f45a40d3e8870d0b923d9d4f4
Jessica Giraldo <jessicacgiraldo@gmail.com>
2023-05-25 10:36:20 -0400

    submissionsone

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function addingEventListener() {
-    const input = document.getElementById('button');
+const event = document.getElementById('button')
 
-function clickAlert() {
-  alert('I was clicked!');
+
+function click() {
+  alert('I was clicked!')
 }
 
-input.addEventListener('click', clickAlert);
+event.addEventListener('click' , click );
 }
